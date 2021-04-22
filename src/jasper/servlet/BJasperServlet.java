5f9f801da58d6514ea42e38e1ded16e23ac37fb0
Andy Frank <afrankvt@gmail.com>
2021-04-22 13:36:03 -0400

    Flush out basic URI parsing and service/error handling

--- src/jasper/servlet/BJasperServlet.java
+++ src/jasper/servlet/BJasperServlet.java
@@ -11,6 +11,7 @@ package jasper.servlet;
 import java.io.*;
 import javax.baja.io.*;
 import javax.baja.sys.*;
+import javax.baja.util.*;
 import javax.baja.web.*;
 import javax.servlet.http.*;
 
@@ -22,15 +23,8 @@ public final class BJasperServlet extends BWebServlet
   /*-
   class BJasperServlet
   {
-    properties
-    {
-    }
-
-    actions
-    {
-    }
   }
-   -*/
+  -*/
 /*+ ------------ BEGIN BAJA AUTO GENERATED CODE ------------ +*/
 /*@ $jasper.servlet.BJasperServlet(2852803851)1.0$ @*/
 /* Generated Wed Apr 21 15:57:23 EDT 2021 by Slot-o-Matic 2000 (c) Tridium, Inc. 2000 */
@@ -44,9 +38,10 @@ public final class BJasperServlet extends BWebServlet
 
 /*+ ------------ END BAJA AUTO GENERATED CODE -------------- +*/
 
-  /**
-   * Default constructor
-   */
+////////////////////////////////////////////////////////////////
+// Constructor
+////////////////////////////////////////////////////////////////
+
   public BJasperServlet()
   {
     super.setServletName("jasper");
@@ -57,18 +52,64 @@ public final class BJasperServlet extends BWebServlet
 // Servlet
 ////////////////////////////////////////////////////////////////
 
-  public void doGet(WebOp op) throws IOException
-  {
-    HttpServletResponse res = op.getResponse();
-    res.setHeader("Content-Type", "text/plain");
-    res.setStatus(200);
-
-    HtmlWriter out = op.getHtmlWriter();
-    out.w("Hello, World! from Jasper!");
-    out.flush();
-  }
-
   public void doPost(WebOp op) throws IOException
   {
+    HttpServletRequest req = op.getRequest();
+    String[] path = splitPath(req.getPathInfo());
+
+    // sanity check path is long enough
+    if (path.length < 3) { sendNotFound(op); return; }
+
+    // key off version
+    if (path[1].equals("v1"))
+    {
+      if (path[2].equals("points")) { doPoints(op); return; }
+    }
+
+    // if we get here there 404
+    sendNotFound(op);
+  }
+
+  /** Service /v1/points request. */
+  private void doPoints(WebOp op) throws IOException
+  {
+    HttpServletResponse res = op.getResponse();
+    res.setStatus(200);
+    res.setHeader("Content-Type", "application/json");
+    op.getHtmlWriter().w("{\"msg\":\"Hello, World!\"}").flush();
+  }
+
+  /** Convenience for sendErr(404) */
+  private void sendNotFound(WebOp op) throws IOException
+  {
+    sendErr(op, 404, "Not Found");
+  }
+
+  /** Send an error repsponse as JSON with code and msg. */
+  private void sendErr(WebOp op, int code, String msg) throws IOException
+  {
+    HttpServletResponse res = op.getResponse();
+    res.setStatus(code);
+    res.setHeader("Content-Type", "application/json");
+    op.getHtmlWriter().w("{\"err_msg\":\"").safe(msg).w("\"}").flush();
+  }
+
+  /** Split a path string into array. */
+  private String[] splitPath(String path)
+  {
+    // TODO FIXIT: there _has_ to be a better way todo this????
+    String[] orig = TextUtil.splitAndTrim(path, '/');
+
+    // get non-empty size
+    int size = 0;
+    for (int i=0; i<orig.length; i++)
+      if (orig[i].length() > 0) size++;
+
+    // filter out empty
+    String[] acc = new String[size];
+    int p = 0;
+    for (int i=0; i<orig.length; i++)
+      if (orig[i].length() > 0) acc[p++] = orig[i];
+    return acc;
   }
 }
